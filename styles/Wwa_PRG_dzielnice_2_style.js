bc379bdd4be2dea0aa17008cf643bc93b631a4a7
kyrylo-zharkovskyi <kyrylo.zharkovskyi@pw.edu.pl>
2024-11-27 18:54:42 +0100

    Add files via upload

---
+++ styles/Wwa_PRG_dzielnice_2_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Wwa_PRG_dzielnice_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "26.0px \'Arial\', sans-serif";
+    var labelFill = "#888888";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("JPT_NAZWA_") !== null) {
+        labelText = String(feature.get("JPT_NAZWA_"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.8)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 3.04}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
